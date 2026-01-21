4d153e44f0e80fa29bc4e4afe2d105af7bab9094
yuanj14 <1959558509@qq.com>
2026-01-21 15:04:59 +0800

    test(Task): 流程变量动态指定处理人

--- src/test/java/com/czu/workflow/TaskAssigneeTest.java
+++ src/test/java/com/czu/workflow/TaskAssigneeTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
 import java.util.List;
 
 @SpringBootTest(classes = Application.class)
@@ -31,8 +32,8 @@ public class TaskAssigneeTest {
     @Test
     public void deployFlow() {
         Deployment deploy = repositoryService.createDeployment()
-                .addClasspathResource("static/processResource/flow/任务分配-Assignee.bpmn")
-                .name("任务分配-Assignee部署")
+                .addClasspathResource("static/processResource/flow/任务分配-值表达式.bpmn")
+                .name("任务分配-值表达式部署")
                 .deploy();
         System.out.println("deploy.getId() = " + deploy.getId());
         System.out.println("deploy.getName() = " + deploy.getName());
@@ -40,6 +41,7 @@ public class TaskAssigneeTest {
 
     /**
      * 启动流程实例 BY KEY
+     * 用于启动最新版本流程实例
      */
     @Test
     public void startProcessInstanceByKey() {
@@ -47,20 +49,37 @@ public class TaskAssigneeTest {
         // - key: 对应 ACT_RE_PROCDEF 表的 KEY_ 字段（流程定义的key）
         // - 返回的 ProcessInstance 的 id: 对应 ACT_RU_EXECUTION 表的 ID_ 字段（流程实例ID）
         //   同时也对应 ACT_HI_PROCINST 表的 PROC_INST_ID_ 字段（历史流程实例ID）
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("Process_0a0q6t3");
+        String processDefinitionKey = "Process_0a0q6t3";
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(processDefinitionKey);
         System.out.println("流程实例ID (ACT_RU_EXECUTION.ID_): " + processInstance.getId());
         System.out.println("流程定义KEY (ACT_RE_PROCDEF.KEY_): " + processInstance.getProcessDefinitionId());
     }
 
     /**
      * 启动流程实例 BY ID
+     * 通过流程变量分配处理人
+     * NEXT 状态变量需要指定
+     */
+    @Test
+    public void startProcessInstanceByIdWithVariables() {
+        String processDefinitionId = "Process_0a0q6t3:4:26ee1289-f692-11f0-821e-30560f01377c";
+        HashMap<String, Object> variables = new HashMap<>();
+        variables.put("user1", "demo");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinitionId, variables);
+        System.out.println("流程实例ID (ACT_RU_EXECUTION.ID_): " + processInstance.getId());
+    }
+
+    /**
+     * 启动流程实例 BY ID
+     * 用于启动旧版本流程实例
      */
     @Test
     public void startProcessInstanceById() {
-        ProcessInstance processInstance = runtimeService.startProcessInstanceById("Process_0a0q6t3:2:42620b84-f600-11f0-a5d5-30560f01377c");
+        String processDefinitionId = "Process_0a0q6t3:1:42620b84-f600-11f0-a5d5-30560f01377c";
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinitionId);
         System.out.println("流程实例ID (ACT_RU_EXECUTION.ID_): " + processInstance.getId());
-        System.out.println("流程定义KEY (ACT_RE_PROCDEF.KEY_): " + processInstance.getProcessDefinitionId());
     }
+
     /**
      * 查询任务
      */
@@ -83,8 +102,10 @@ public class TaskAssigneeTest {
     @Test
     public void completeTask() {
         // taskId 对应的是 Camunda 数据库中的 ACT_RU_TASK 表的 ID_ 字段
-        String taskId = "b347893f-f603-11f0-b87d-30560f01377c";
-        taskService.complete(taskId);
+        String taskId = "f105514c-f696-11f0-9388-30560f01377c";
+        HashMap<String, Object> variables = new HashMap<>();
+        variables.put("user2", "demo");
+        taskService.complete(taskId, variables);
         System.out.println("审批完成，任务ID: " + taskId + " (对应表 ACT_RU_TASK 中的 ID_ 字段)");
     }
 }
